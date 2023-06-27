48d7ebf6ebce3ab1a01524837251061d54d10ad3
stivex001 <stephenadeyemo@gmail.com>
2023-06-27 18:22:12 +0100

    feat: added ew notes to the array

--- PartOne/src/App.jsx
+++ PartOne/src/App.jsx
@@ -1,18 +1,42 @@
-import Note from "./components/Note"
+import { useState } from "react";
+import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (prop) => {
+  const [notes, setNotes] = useState(prop.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         <ul>
-          {notes.map(note => 
+          {notes.map((note) => (
             <Note key={note.id} note={note} />
-          )}
+          ))}
         </ul>
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
