bfe98d1ab30e1e0d33755c858b2961c9452f2388
stivex001 <stephenadeyemo@gmail.com>
2023-06-28 22:56:21 +0100

    fix: typo fixed

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,4 @@ const PORT = process.env.PORT;
 
 // LISTEN TO REQUEST ON PORT 5000
 
-app.listen(PORT, () => logger.info(`app running on port ${PORT}`));
+app.listen(PORT, () =>(`app running on port ${PORT}`));
