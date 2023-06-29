bd8ba92884f7c5a2eb387c8385d504cd1ea11184
stivex001 <stephenadeyemo@gmail.com>
2023-06-29 18:54:26 +0100

    started the backend server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,38 @@
-const app = require("./app");
+const express = require('express')
+const app = express()
+
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
 
 // PORT
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000 ;
 
 // LISTEN TO REQUEST ON PORT 5000
 
-app.listen(PORT, () =>(`app running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
