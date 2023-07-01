d11acbd67f19182604a8876bcc1e3440223e8992
stivex001 <stephenadeyemo@gmail.com>
2023-07-01 13:20:13 +0100

    feat:created the people schema

---
+++ backend/model/people.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const peopleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    number: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const People = mongoose.model("People", peopleSchema);
+export default People;
