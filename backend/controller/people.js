b225e248eec6ca8f14bcece57348354427d5cf1b
stivex001 <stephenadeyemo@gmail.com>
2023-07-01 13:46:00 +0100

    implemented creating new person logic

--- backend/controller/people.js
+++ backend/controller/people.js
@@ -1,3 +1,28 @@
+import People from "../model/people.js";
+
 export const getPeople = (req, res) => {
-    res.status(200).json("sucess")
-}
+  res.status(200).json("sucess");
+};
+
+export const addPeople = async (req, res) => {
+  try {
+    const { name, number } = req.body;
+
+    // check if the person is already in the databse
+    const existingPerson = await People.findOne({ name });
+
+    if (existingPerson) {
+      return res.status(409).json({
+        error: "Person already exists",
+      });
+    }
+
+    // creating a new person data
+    const newPerson = await new People({ name, number }).save();
+
+    res.status(201).json({message: "Person created Successfully"})
+
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/peopleRoute.js
+++ backend/routes/peopleRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { getPeople } from "../controller/people.js"
+import { addPeople, getPeople } from "../controller/people.js"
 
 const router = express.Router()
 
 router.get('/', getPeople)
+
+router.post("/addPerson", addPeople)
 
 export default router;
